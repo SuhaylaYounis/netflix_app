d16fd17053a9dbdf168799140f26a251f92ae911
SuhaylaYounis <60473969+SuhaylaYounis@users.noreply.github.com>
2021-10-20 11:19:37 +0200

    .
    .

--- netflix/lib/screens/Discription_screens/disc_screen.dart
+++ netflix/lib/screens/Discription_screens/disc_screen.dart
@@ -1,8 +1,7 @@
 import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:netflix/screens/web_view_screen.dart';
 import '../../constants.dart';
 
 class DiscScreen extends StatelessWidget {
@@ -11,7 +10,6 @@ class DiscScreen extends StatelessWidget {
   String overview;
   String title;
   String poster_path;
-
 
   DiscScreen(
       {required this.id,
@@ -23,60 +21,111 @@ class DiscScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Stack(
+      body: Stack(
         fit: StackFit.expand,
         children: [
-          Image.network(image_url+poster_path,fit:BoxFit.fill),
-          BackdropFilter(filter:ImageFilter.blur(
-            sigmaX:5,sigmaY:5,
-          ),
-          child:Container(
+          Image.network(image_url + poster_path, fit: BoxFit.fill),
+          BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 5,
+              sigmaY: 5,
+            ),
+            child: Container(
               color: Colors.white.withOpacity(0.2),
-          ),
+            ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal:10.0, vertical: 10.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height/20,
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height/1.5,
-                  width:MediaQuery.of(context).size.width ,
-                  alignment: Alignment.center,
-                  child: Image.network(
-                    image_url+poster_path,
-                    fit: BoxFit.fitWidth,
-
+            padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 30,
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: Container(
-                    child: Center(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 70,
-                            child: Text("$title",
-                            style: TextStyle(color: white, fontSize: 30),),
-                          ),
-                          Expanded(
-                            flex: 25,
-                            child: Text("$vote_average /10",
-                              style: TextStyle(color: white, fontSize: 30),),
-                          ),
-                        ],
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: TextButton.icon(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: white,
+                        size: 30,
+                      ),
+                      label: Text(
+                        'back',
+                        style: TextStyle(
+                            color: white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w800),
                       ),
                     ),
                   ),
-                )
-              ],
+                  Container(
+                    height: MediaQuery.of(context).size.height / 2,
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.center,
+                    child: Image.network(
+                      image_url + poster_path,
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Container(
+                      child: Center(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 70,
+                              child: Text(
+                                "$title",
+                                style: TextStyle(color: white, fontSize: 30),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 25,
+                              child: Text(
+                                "$vote_average /10",
+                                style: TextStyle(color: white, fontSize: 30),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                    child: Text(
+                      "$overview",
+                      style: TextStyle(color: white, fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
             ),
           )
         ],
-      )
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (BuildContext context) {
+            return WebViewScreen(widget.title);
+          }));
+        },
+        label: Text(
+          " Watch on Egybest",
+          style: TextStyle(fontSize: 16),
+        ),
+        elevation: 10,
+        backgroundColor: red,
+        autofocus: true,
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
     );
   }
 }
---
+++ netflix/lib/screens/web_view_screen.dart
@@ -0,0 +1,168 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:netflix/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+class WebViewScreen extends StatefulWidget {
+  String title;
+
+  WebViewScreen(this.title);
+
+  @override
+  _WebViewScreenState createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+      ));
+
+  late PullToRefreshController pullToRefreshController;
+  String url = "";
+  double progress = 0;
+  final urlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    pullToRefreshController = PullToRefreshController(
+      options: PullToRefreshOptions(
+        color: Colors.blue,
+      ),
+      onRefresh: () async {
+        if (Platform.isAndroid) {
+          webViewController?.reload();
+        } else if (Platform.isIOS) {
+          webViewController?.loadUrl(
+              urlRequest: URLRequest(url: await webViewController?.getUrl()));
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: black,
+        appBar:  AppBar(
+          backgroundColor: black,
+          elevation: 3,
+          centerTitle: true,
+          title: Text(
+            "${widget.title}",
+            style: TextStyle(color: primarycolor, fontWeight: FontWeight.bold),
+          ),
+          leading: IconButton(onPressed: () {
+            Navigator.pop(context);
+
+          }, icon:    Icon(
+            Icons.arrow_back_ios,
+            color: primarycolor,
+          ),),
+        ),
+        body:  SafeArea(
+            child: Column(children: <Widget>[
+
+              Expanded(
+                child: Stack(
+                  children: [
+                    InAppWebView(
+                      key: webViewKey,
+                      initialUrlRequest:
+                      URLRequest(url: Uri.parse("https://faze.egybest.pet/movie/${widget.title}")),
+                      initialOptions: options,
+                      pullToRefreshController: pullToRefreshController,
+                      onWebViewCreated: (controller) {
+                        webViewController = controller;
+                      },
+                      onLoadStart: (controller, url) {
+                        setState(() {
+                          this.url = url.toString();
+                          urlController.text = this.url;
+                        });
+                      },
+                      androidOnPermissionRequest: (controller, origin, resources) async {
+                        return PermissionRequestResponse(
+                            resources: resources,
+                            action: PermissionRequestResponseAction.GRANT);
+                      },
+                      shouldOverrideUrlLoading: (controller, navigationAction) async {
+                        var uri = navigationAction.request.url!;
+
+                        if (![ "http", "https", "file", "chrome",
+                          "data", "javascript", "about"].contains(uri.scheme)) {
+                          if (await canLaunch(url)) {
+                            // Launch the App
+                            await launch(
+                              url,
+                            );
+                            // and cancel the request
+                            return NavigationActionPolicy.CANCEL;
+                          }
+                        }
+
+                        return NavigationActionPolicy.ALLOW;
+                      },
+                      onLoadStop: (controller, url) async {
+                        pullToRefreshController.endRefreshing();
+                        setState(() {
+                          this.url = url.toString();
+                          urlController.text = this.url;
+                        });
+                      },
+                      onLoadError: (controller, url, code, message) {
+                        pullToRefreshController.endRefreshing();
+                      },
+                      onProgressChanged: (controller, progress) {
+                        if (progress == 100) {
+                          pullToRefreshController.endRefreshing();
+                        }
+                        setState(() {
+                          this.progress = progress / 100;
+                          urlController.text = this.url;
+                        });
+                      },
+                      onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                        setState(() {
+                          this.url = url.toString();
+                          urlController.text = this.url;
+                        });
+                      },
+                      onConsoleMessage: (controller, consoleMessage) {
+                        print(consoleMessage);
+                      },
+                    ),
+                    progress < 1.0
+                        ? LinearProgressIndicator(value: progress)
+                        : Container(),
+                  ],
+                ),
+              ),
+
+            ])),
+
+
+      ),
+    );
+  }
+}
+
